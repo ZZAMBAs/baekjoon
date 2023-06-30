de3be6fa44fb36fc891446255bc827aeb2e3815f
ZZAMBAs <rksidksrksi@naver.com>
2023-06-30 12:47:27 +0900

    problem_17358

---
+++ math/problem_17358.cpp
@@ -0,0 +1,20 @@
+/*
+//
+// Created by cho on 23. 6. 30.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long N, res = 1;
+    cin >> N;
+    N--;
+     do{
+        res = (res * N) % 1000000007LL;
+        N -= 2;
+    } while (N > 1);
+
+     cout << res;
+    return 0;
+}*/
